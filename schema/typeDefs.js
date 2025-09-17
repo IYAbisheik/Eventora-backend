263e2d5889f56b038868c14249444ae46fc1024b
IYabisheik <iraiyarulabisheik.a@mitrahsoft.com>
2025-09-17 20:44:46 +0530

    Integrated the updateUser

--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -4,12 +4,26 @@ const typeDefs = gql`
   type User {
     id: ID!
     email: String!
+    firstname: String
+    lastname: String
+    username: String
+    phoneNumber: String
+    birthDate: String
+    gender: String
     token: String
   }
 
   type Mutation {
     register(email: String!, password: String!): User
     login(email: String!, password: String!): User
+    updateUser(
+      firstname: String
+      lastname: String
+      username: String
+      phoneNumber: String
+      birthDate: String
+      gender: String
+    ): User
   }
 
   type Query {
